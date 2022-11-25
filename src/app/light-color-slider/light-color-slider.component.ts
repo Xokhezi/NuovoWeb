3ef69ef93480dc8b662f7ea252bd0bb444cb232b
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2022-11-25 14:08:50 +0100

    Product slider to HomeScreen, minor design fixes

--- src/app/light-color-slider/light-color-slider.component.ts
+++ src/app/light-color-slider/light-color-slider.component.ts
@@ -1,3 +1,4 @@
+import { ProductsService } from './../services/products.service';
 import { fade, fadeSideBlue, fadeSideRed, fadeSlider } from './light-color.slider.animations';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,36 +12,54 @@ export interface Tile {
   selector: 'light-color-slider',
   templateUrl: './light-color-slider.component.html',
   styleUrls: ['./light-color-slider.component.css'],
-  animations:[
+  animations: [
     fade,
     fadeSideRed,
     fadeSideBlue,
     fadeSlider
-    
+
   ]
 })
 export class LightColorSliderComponent implements OnInit {
- state:any;
-  constructor() { 
-    this.state="default";
+  state: any;
+  id = 0;
+  product: any;
+  products: any;
+  constructor(private service: ProductsService) {
+    this.state = "default";
+    this.products = service.getProducts();
+    this.product = service.getCategory('red')[0];
   }
 
   ngOnInit(): void {
   }
 
-  switchState(input:any)
-  {
-    this.state=input;
+  switchState(input: any) {
+    this.state = input;
+    this.id = 0;
+    this.product = this.service.getCategory(input)[0];
     console.log(this.state)
   }
-  startTour()
-{
-  this.state='red';
-}
-test()
-{
-  this.state='test';
-  console.log();
-}
+  startTour() {
+    this.state = 'red';
+  }
+  switchProduct(method: any, category: any) {
+    let prods: any;
 
+    if (category === 'red')
+      prods = this.service.getCategory('red');
+    else if (category === 'blue')
+      prods = this.service.getCategory('blue');
+
+    if (method === 'next') {
+      if (this.id < prods.length - 1)
+        this.id++
+    }
+    else if (method === 'previous') {
+      if (this.id != 0)
+        this.id--;
+    }
+
+    this.product = prods[this.id];
+    }
 }
